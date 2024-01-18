1beea6fdf9d10068051523baca6b762e30027b7a
student1 <student1@C7A220-N2>
2024-01-18 09:34:16 +0300

    Я добавил имя

--- Спиридонов/Program.cs
+++ Спиридонов/Program.cs
@@ -1,3 +1,3 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-("Hello, World! spiridonov")
+Console.WriteLine("Hello,spiridonov!");
+Console.WriteLine("Hello,maksim!");
